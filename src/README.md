4973632799542a54a047d96d788c6bcc6f986c6c
Alfonso Beltran <aebeltran13@gmail.com>
2020-06-30 15:34:03 -0600

    Redoing UI. Updated README.

--- src/README.md
+++ src/README.md
@@ -12,4 +12,9 @@ Run token_test(./token_test) in order to start the tokenizer.
 -When printing tokens, error message "Segmentation fault (core dumped)"
  appears. This is because print_tokens is trying to access information that is
  not there.
--history.c has many warnings due to pointer assignment issues.
+
+#Resources
+
+Needed a way to compare string to make the user interface and I found a great
+website that explains how strcomp() works in C.
+-[This website is great](https://overiq.com/c-programming-101/the-strcmp-function-in-c/#:~:text=Syntax%3A%20int%20strcmp%20(const%20char,ASCII%20value%20of%20the%20characters.)
--- src/main.c
+++ src/main.c
@@ -4,43 +4,58 @@
 #include "history.h"
 #define MAX 64
 
-/* Small set of strings to introduce the program */
-void intro(){
-  printf("Tokenizer Project for Arch1\n\n=========================\n");
-  printf("Enter a string to start:\n");
-}
+/*Declaring str_compare*/
+int str_compare(char *str1, char *str2);
 
 /* MAIN FUNC */
 int main(){
   char string[MAX];
-  char user_choice; /* Users choice to continue runnin the file */
-  char cont = 'y'; /* Continue variable */
-  /*
-  int id;
-  List *list = init_history();
-*/
-  intro();
+  char user_choice[] = "AGN"; /* Users choice to continue running the file */
+  int Power_on = 1;
 
-  while(cont == 'y'){
-    printf("> ");
-    scanf("%[^\n]%*c", string); /* %[^\n] will scan until it finds new line*/
-    printf("%s\n", string);
-    /*    add_history(list, string); */
-    char **tokens = tokenize(string);
-    print_tokens(tokens);
-    /*
-    printf("Type 'y' for another sentence, 'h' for history, 'n' to quit\n");
-    scanf("%s\n", user_choice);
-    if(user_choice == 'y') continue;
-    if(user_choice == 'h'){
-      print_history(list);
-      scanf("Type id: %d\n", id);
-      string = get_history(list, id);
+  printf("  Project 1: TOKENIZER\n++++++++++++++++++++++++\n\n");
+  
+  while(Power_on != 0){
+    if(str_compare(user_choice, "AGN") == 0){ /*AGN Again: Enter a new String*/
+      printf("Enter a string(64 characters MAX)\n");
+      printf("> ");
+      /*scanf("%[^\n]%*c", string); /* %[^\n] will scan until it finds new line*/
+      fgets(string, sizeof(string), stdin);
+    }
+    if(str_compare(user_choice, "HST") == 0){ /*HST History*/
+      /*Printing history and getting previously added string*/
+      printf("in History");
+    }
+    if(str_compare(user_choice, "EOP") == 0){ /*EOP End Of Programm*/
+      printf("Thanks, Bye!");
+      break;
+    }else{
+      printf("Unknown Command.");
+      /*Enter user_choice funciton here*/
       continue;
     }
-    if(user_choice == 'n') break;
-    */
+
+    /*After getting a string we tokenize and print tokens. Ask for user_choice*/
+    printf("%s\n", string);
+    /*tokenize and print tokenz here*/
+    
+    printf("Enter Command: \"AGN\" to enter a new string,"
+	   + "\"HST\" to view history, \"EOP\" to quit.\n");
+    
   }
-  
+   
   return 0;
+}
+
+int str_compare(char *str1, char *str2){
+  while((*str1 != '\0' && *str2 != '\0') && *str1 == *str2){
+    str1++;
+    str2++;
+  }
+  if(*str1 == *str2){
+    return 0;
+  }
+  else{
+    return *str1 - *str2;
+  }
 }
